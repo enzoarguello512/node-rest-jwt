ccfee73e1ab3c3dab541a0c19aa29afcd3f825c1
Enzo Arguello <arguelar@gmail.com>
2022-09-09 16:13:52 -0300

    fix: update interfaces and models.

--- components/user/dto/create.user.dto.ts
+++ components/user/dto/create.user.dto.ts
@@ -2,20 +2,11 @@ import mongoose from 'mongoose';
 
 export interface ICreateUserDto extends mongoose.Document {
   id: string;
-  email: string;
-  password: string;
   firstName: string;
   lastName?: string;
+  email: string;
+  password: string;
   permissionLevel: number;
-  age: number;
-  address?: string;
-  country: string;
-  city?: string;
-  zipCode?: string;
-  state?: string;
-  phoneNumber: string;
-  avatar: string;
-  avatarId: string;
   createdAt?: Date;
   updatedAt?: Date;
   refreshToken?: Array<string>;
