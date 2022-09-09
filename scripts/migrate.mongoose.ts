8036ca2ac8157bdfd2a84988bd2fc5c3b1ccdd62
Enzo Arguello <arguelar@gmail.com>
2022-09-09 20:39:20 -0300

    fix: update insert mode.

--- scripts/migrate.mongoose.ts
+++ scripts/migrate.mongoose.ts
@@ -3,7 +3,9 @@ import { products } from './products.data';
 
 (async () => {
   await Product.deleteMany({});
-  await Product.insertMany(products);
+  await Product.insertMany(products, {
+    ordered: true,
+  });
   console.log(`Inserted ${products.length} products`);
   process.exit(1);
 })();
