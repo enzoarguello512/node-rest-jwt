9daca042805ef49cb4ccb9482f3aef5f85db0b1b
Enzo Arguello <arguelar@gmail.com>
2022-10-28 11:37:57 -0300

    fix: add config variable settings.
    The added variables will be used for the new services.

--- src/config/production.ts
+++ src/config/production.ts
@@ -32,6 +32,22 @@ const defaultConfig = {
     accesstoken: process.env.JWT_ACCESS_TOKEN_SECRET || 'secret',
     refreshtoken: process.env.JWT_REFRESH_TOKEN_SECRET || 'secret',
   },
+  cloudinary: {
+    cloudname: process.env.CLOUDINARY_CLOUD_NAME || 'cloudname',
+    apikey: process.env.CLOUDINARY_API_KEY || 'apikey',
+    apisecret: process.env.CLOUDINARY_API_SECRET || 'apisecret',
+  },
+  google: {
+    email: process.env.GOOGLE_EMAIL || 'email',
+    password: process.env.GOOGLE_PASSWORD || 'password',
+  },
+  twilio: {
+    accountsid: process.env.TWILIO_ACCOUNT_SID || 'accountsid',
+    authtoken: process.env.TWILIO_AUTH_TOKEN || 'authtoken',
+    servicenumber: process.env.TWILIO_SERVICE_PHONE_NUMBER || 'servicenumber',
+    receivernumber:
+      process.env.TWILIO_RECEIVER_PHONE_NUMBER || 'receivernumber',
+  },
   databases: {
     mongolocal: {
       port: process.env.MONGO_LOCAL_PORT || 27017,
