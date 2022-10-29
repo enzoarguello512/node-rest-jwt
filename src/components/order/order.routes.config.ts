350c5c6fd5c6adec8fe7daa268aa6c0f91aaf24d
Enzo Arguello <arguelar@gmail.com>
2022-10-29 10:16:18 -0300

    fix: update endpoint to create order.

--- src/components/order/order.routes.config.ts
+++ src/components/order/order.routes.config.ts
@@ -1,6 +1,8 @@
 import CommonRoutesConfig from '../../common/common.routes.config';
 import OrdersController from './controllers/order.controller';
 import OrdersMiddleware from './middleware/order.middleware';
+import UsersMiddleware from '../user/middleware/user.middleware';
+import CartMiddleware from '../cart/middleware/cart.middleware';
 import express from 'express';
 import PermissionMiddleware from '../../common/middleware/common.permission.middleware';
 
@@ -9,12 +11,16 @@ export default class OrdersRoutes extends CommonRoutesConfig {
     super(app, 'OrdersRoutes');
   }
   configureRoutes(): express.Application {
-    this.app.route(`/orders`).get(OrdersController.listOrders).post(
+    this.app.route(`/orders`).get(OrdersController.listOrders);
+
+    this.app.post(`/orders/:userId/cart/:cartId`, [
       OrdersMiddleware.validateRequiredOrderBodyFields,
       // PermissionMiddleware.isAdmin,
+      UsersMiddleware.validateUserExists,
+      CartMiddleware.validateCartExists,
       OrdersMiddleware.transformOrder,
-      OrdersController.createOrder
-    );
+      OrdersController.createOrder,
+    ]);
 
     this.app.param(`orderId`, OrdersMiddleware.extractOrderId);
     this.app
