2ce6dc8b60f74eff6c95134c5e9f4d9a0b9816c1
Enzo Arguello <arguelar@gmail.com>
2022-11-07 16:02:16 -0300

    docs: add comments to properties.

--- src/components/product/dto/create.product.dto.ts
+++ src/components/product/dto/create.product.dto.ts
@@ -6,11 +6,12 @@ export interface ICreateProductDto extends mongoose.Document {
   name: string;
   description: string;
   productCode?: number;
-  imageId: string | UploadedFile;
+  imageId: string | UploadedFile; // it is of type UploadedFile when the user makes the request (when the image is going to be uploaded) and takes the value of string when it is saved in the database
+  // e.g "Products/dnaar7qtosptxztq7apv" or an image
   imageUrl: string;
   hasFreeShipping: boolean;
   discount: number;
-  discountedPrice: number;
+  discountedPrice: number; // automatically calculated by mongoose
   promotion?: Array<string>;
   categories?: Array<string>;
   region?: Array<string>;
@@ -18,6 +19,6 @@ export interface ICreateProductDto extends mongoose.Document {
   payment?: Array<string>;
   price: number;
   stock: number;
-  createdAt?: Date;
-  updatedAt?: Date;
+  createdAt?: Date; // automatically added by mongoose
+  updatedAt?: Date; // automatically added by mongoose
 }
