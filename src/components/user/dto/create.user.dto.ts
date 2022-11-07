5e2dd95cbc4d94d35118ad84bfa329bccf4ca926
Enzo Arguello <arguelar@gmail.com>
2022-11-07 17:58:42 -0300

    docs: add comments to properties.

--- src/components/user/dto/create.user.dto.ts
+++ src/components/user/dto/create.user.dto.ts
@@ -11,11 +11,13 @@ export interface ICreateUserDto extends mongoose.Document {
   address: string;
   age: number;
   phoneNumber: string;
-  imageId: string | UploadedFile;
+  imageId: string | UploadedFile; // it is of type UploadedFile when the user makes the request (when the image is going to be uploaded) and takes the value of string when it is saved in the database
+  // e.g "Products/dnaar7qtosptxztq7apv" or an image
+  // If an image is not selected, the default image will be used.
   imageUrl: string;
   permissionLevel: number;
   refreshToken?: Array<string>;
   cart?: mongoose.Types.ObjectId | ICreateCartDto;
-  createdAt?: Date;
-  updatedAt?: Date;
+  createdAt?: Date; // automatically added by mongoose
+  updatedAt?: Date; // automatically added by mongoose
 }
