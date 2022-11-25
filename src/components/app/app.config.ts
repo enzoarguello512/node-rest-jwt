6bdf91a7b5b616c171f7cbbfb64297c7f5a7aceb
Enzo Arguello <arguelar@gmail.com>
2022-11-25 16:03:23 -0300

    feat: add local origin.

--- src/components/app/app.config.ts
+++ src/components/app/app.config.ts
@@ -3,5 +3,6 @@ export const allowedOrigins = [
   // 'http://127.0.0.1:5500',
   // 'http://localhost:3500',
   'http://localhost:3000',
+  'http://localhost:8080',
   'https://morfi-react.vercel.app',
 ];
