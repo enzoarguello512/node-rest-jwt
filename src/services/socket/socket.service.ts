8a0536af6f8a0d14593a807446896aa2fcdc4a68
Enzo Arguello <arguelar@gmail.com>
2022-12-07 10:46:14 -0300

    fix: update methods to work with redux state.

--- src/services/socket/socket.service.ts
+++ src/services/socket/socket.service.ts
@@ -1,10 +1,8 @@
 import socketio from 'socket.io';
 import debug from 'debug';
 import messageService from '../../components/message/services/message.service';
-import userService from '../../components/user/services/user.service';
-import { IMessage } from '../../common/types/message.interface';
-import { NotFoundError } from '../../common/error/not.found.error';
 import BaseError from '../../common/error/base.error';
+import { ICreateMessageDto } from '../../components/message/dto/create.message.dto';
 
 const log: debug.IDebugger = debug('app:socket-io');
 
@@ -16,8 +14,10 @@ export default class SocketServer {
     this.socket = socket;
 
     // Event handlers
-    socket.on('new message', (message) => this.newMessage(message));
-    socket.on('get messages', (userEmail) => this.getMessages(userEmail));
+    socket.on('new message', (userId: string, messageText: string) =>
+      this.newMessage(userId, messageText)
+    );
+    socket.on('get messages', (userId: string) => this.getMessages(userId));
 
     log('Started socket-io service');
   }
@@ -25,38 +25,47 @@ export default class SocketServer {
   // TODO newMessage and getMessages are very similar methods, redundancies must be eliminated.
 
   // Method responsible for saving messages in the database
-  public async newMessage(message: IMessage): Promise<void> {
+  public async newMessage(userId: string, messageText: string): Promise<void> {
     try {
-      const user = await userService.getUserByEmail(message.email);
-      if (!user) {
-        throw new NotFoundError('User email not found', 'newMessage');
-      }
-      // @ts-ignore
-      await messageService.create({
-        user: user._id,
-        text: message.text,
-        type: 'user',
-      });
+      let userMessage: ICreateMessageDto;
 
-      const messagesList = await messageService.listUserItemsCollection(
-        user._id
+      if (messageText !== 'welcomeMessage') {
+        userMessage = (await messageService.create({
+          // @ts-expect-error
+          user: userId,
+          text: messageText,
+          type: 'user',
+        })) as ICreateMessageDto;
+      }
+
+      const systemResponse = await messageService.generateSystemMessage(
+        userId,
+        messageText
       );
-      this.socket.emit('new message saved', messagesList);
+
+      const serverMessage = (await messageService.create({
+        // @ts-expect-error
+        user: userId,
+        text: systemResponse,
+        type: 'server',
+      })) as ICreateMessageDto;
+
+      if (messageText === 'welcomeMessage') {
+        this.socket.emit('new message saved', serverMessage);
+        return;
+      }
+
+      // @ts-expect-error
+      this.socket.emit('new message saved', [userMessage, serverMessage]);
     } catch (err) {
       const message = err instanceof BaseError ? err.message : err;
       this.socket.emit('messages error', { message });
     }
   }
   // Method responsible for retrieving messages from a specific user
-  public async getMessages(userEmail: string): Promise<void> {
+  public async getMessages(userId: string): Promise<void> {
     try {
-      const user = await userService.getUserByEmail(userEmail);
-      if (!user) {
-        throw new NotFoundError('User email not found', 'getMessages');
-      }
-      const messagesList = await messageService.listUserItemsCollection(
-        user._id
-      );
+      const messagesList = await messageService.listUserItemsCollection(userId);
       this.socket.emit('messages', messagesList);
     } catch (err) {
       const message = err instanceof BaseError ? err.message : err;
