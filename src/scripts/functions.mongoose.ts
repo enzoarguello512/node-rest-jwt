0499a774dc84c250f05924ec678665a45533a725
Enzo Arguello <arguelar@gmail.com>
2022-12-11 16:19:18 -0300

    fix: update populate function with new set.

--- src/scripts/functions.mongoose.ts
+++ src/scripts/functions.mongoose.ts
@@ -1,10 +1,13 @@
 import { Product } from '../components/product/models/product.model';
 import { products } from './products.data';
+import { products as productsv2 } from './products.data-v2';
+
+const itemsToInsert = productsv2;
 
 export const populateMongoDb = async (): Promise<number> => {
-  await Product.deleteMany({});
-  await Product.insertMany(products, {
+  //await Product.deleteMany({});
+  await Product.insertMany(productsv2, {
     ordered: true,
   });
-  return products.length;
+  return itemsToInsert.length;
 };
