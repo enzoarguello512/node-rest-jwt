e44695f8ded29fdf10488805a024c72c4fcfe904
Enzo Arguello <arguelar@gmail.com>
2022-12-15 19:23:24 -0300

    fix: rename patch file.

---
+++ src/docs/paths/products/patchProduct.ts
@@ -0,0 +1,84 @@
+const patchProduct = {
+  patch: {
+    tags: ['Products'],
+    description:
+      'Update an existing product, only available for logged in admin user.',
+    operationId: 'patchProduct',
+    parameters: [
+      {
+        name: 'id',
+        in: 'path',
+        schema: {
+          $ref: '#/components/schemas/ProductId',
+        },
+        required: true,
+        description: 'A single product id',
+      },
+    ],
+    requestBody: {
+      required: 'true',
+      content: {
+        'multipart/form-data': {
+          schema: {
+            $ref: '#/components/schemas/Product',
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: 'Product updated successfully.',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/Product',
+            },
+          },
+        },
+      },
+      400: {
+        description:
+          'One or more of the product properties do not meet the proper conditions.',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/Error',
+            },
+          },
+        },
+      },
+      401: {
+        description: 'Unauthorized route, login first and try again',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/Error',
+            },
+          },
+        },
+      },
+      404: {
+        description: 'The product to update does not exist.',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/Error',
+            },
+          },
+        },
+      },
+      500: {
+        description: 'Server error',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/Error',
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+export default patchProduct;
