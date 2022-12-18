2f9c01be9fe619ddb4606e71beccd734f48c864a
Enzo Arguello <arguelar@gmail.com>
2022-12-17 21:53:54 -0300

    fix: update import broken.

--- src/components/app/middleware/credentials.middleware.ts
+++ src/components/app/middleware/credentials.middleware.ts
@@ -1,4 +1,4 @@
-import { allowedOrigins } from '../app.config';
+import allowedOrigins from '../app.config';
 import express from 'express';
 
 // To include cookies in the request (authentication with JSON Web Tokens)
